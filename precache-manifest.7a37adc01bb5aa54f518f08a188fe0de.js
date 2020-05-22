5fc8e41b5b8afec5d581f991d96f4fa0bd6e7fbe
kiliusz <kilonek@gmail.com>
2020-05-22 13:38:59 +0200

    Updates

---
+++ precache-manifest.7a37adc01bb5aa54f518f08a188fe0de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601a992a56b331a9dbb1e016ee9ebbd0",
+    "url": "/nopressure/index.html"
+  },
+  {
+    "revision": "d499af618b05e56fac77",
+    "url": "/nopressure/static/css/main.2a57acdd.chunk.css"
+  },
+  {
+    "revision": "e83ad6c68ef1db4f28d8",
+    "url": "/nopressure/static/js/2.0c10411c.chunk.js"
+  },
+  {
+    "revision": "ad3b610d0af6a056c886f39746461f0e",
+    "url": "/nopressure/static/js/2.0c10411c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d499af618b05e56fac77",
+    "url": "/nopressure/static/js/main.48934528.chunk.js"
+  },
+  {
+    "revision": "9b858e67076a93b07986",
+    "url": "/nopressure/static/js/runtime-main.c31c5a33.js"
+  },
+  {
+    "revision": "1028740989f5ef7aa22fec212c168907",
+    "url": "/nopressure/static/media/logo.10287409.svg"
+  }
+]);
